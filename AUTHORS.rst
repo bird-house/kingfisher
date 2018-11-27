f429aa6839a67ffeabc86fc8dd7edad65e1f2c93
nilshempelmann <info@nilshempelmann.de>
2018-11-27 16:42:11 +0100

    cookiecutter initial

---
+++ AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Nils Hempelmann <info@nilshempelmann.de>
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ kingfisher/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Kingfisher."""
+
+from .wsgi import application
+
+__author__ = """Nils Hempelmann"""
+__email__ = 'info@nilshempelmann.de'
+__version__ = '0.1.0'
