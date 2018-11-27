3155458a7322364af7b74febd731c5fa240acd4e
nilshempelmann <info@nilshempelmann.de>
2018-11-27 20:18:08 +0100

    initial integration of COP_searchProcess

--- kingfisher/processes/__init__.py
+++ kingfisher/processes/__init__.py
@@ -1,5 +1,12 @@
 from .wps_say_hello import SayHello
+from .wps_COP_search import COP_searchProcess
+# from .wps_COP_fetch import EO_COP_fetchProcess
+# from .wps_COP_indices import EO_COP_indicesProcess
+
 
 processes = [
     SayHello(),
+    COP_searchProcess(),
+    # COP_fetchProcess(),
+    # COP_indicesProcess(),
 ]
